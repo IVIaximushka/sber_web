9972ffb82a411d2d747ac50f186dd1349eea4dd7
Max <dmitmax2002@yandex.ru>
2024-11-09 20:26:14 +0300

    add start project

---
+++ bro.config.js
@@ -0,0 +1,22 @@
+const pkg = require("./package");
+
+module.exports = {
+  apiPath: "stubs/api",
+  webpackConfig: {
+    output: {
+      publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`,
+    },
+  },
+  /* use https://admin.bro-js.ru/ to create config, navigations and features */
+  navigations: {
+    "sber_web.main": "/sber_web",
+  },
+  features: {
+    "sber_web": {
+      // add your features here in the format [featureName]: { value: string }
+    },
+  },
+  config: {
+    "sber_web.api": "/api",
+  },
+};
---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>Hello world для проекта - sber_web</h1>
+  );
+};
+
+export default App;
