3464a8fddccaa2325dc25961c26f14b0e65d9b3e
unknown <dar5114@mail.ru>
2024-12-27 16:52:35 +0300

    list_style/add cards and masonry

---
+++ src/container/list/components/item.style.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const ItemStyled = styled.div`
+    color: #f19502;
+    background-color: rgb(0 0 0 / 87%);
+`;
+
+export const TitleStyled = styled.div`
+    font-size: 2rem;
+    color: #d7e4b2;
+`;
---
+++ src/container/list/components/masonry.style.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const MasonryStyled = styled.div`
+    background-color: rgba(16, 16, 16, 0.11);
+    border-radius: 30px;
+    min-height: 100vh;
+    padding-top: 1.5vh;
+    display: flex;
+    justify-content: center;
+`;
