ecae6c4cdc9df3f1315368a15f3fdce799761f00
Chopar5115 <dar5114@mail.ru>
2025-02-01 11:55:53 +0300

    style_refactor/add animated background

--- src/container/main/components/Main/index.style.tsx
+++ src/container/main/components/Main/index.style.tsx
@@ -5,4 +5,32 @@ export const MainStyled = styled.main`
     flex-grow: 1;
     display: flex;
     justify-content: center;
+
+    background-image: linear-gradient(to right top, #f19502, #c3612b, #873a2f, #451f23, #000000);
+    background-size: 400% 400%;
+    animation: AnimateBG 10s ease infinite;
+
+    @-webkit-keyframes AnimateBG {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+
+    @keyframes Animation {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
 `;
